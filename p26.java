392736f8cf3abb36e8021babbd408fe6568f9e9a
JoseyChuang <129434458+JoseyChuang@users.noreply.github.com>
2024-10-31 01:17:03 +0800

    Add files via upload

---
+++ p26.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+public class p26 {
+    public static void main(String[] args)throws IOException {
+        int i=1;
+		
+        while(i<=5)
+        {
+            System.out.println("第"+i+"次的迴圈");
+            i+=1;
+        }
+        System.out.println("迴圈結束");
+    }
+}
---
+++ p30.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+public class p30 {
+    public static void main(String[] args)throws IOException {
+		
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=1;j<=3;j++)
+            {
+                System.out.println("i圈i是："+i+"\nj是："+j);
+            }
+        }
+        System.out.println("迴圈結束");
+    }
+}
